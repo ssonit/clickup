2ac06c14ce047ee27bb8a43e0781af29050b89f6
sonit <88572152+ssonit@users.noreply.github.com>
2023-12-18 13:41:31 +0700

    Update src/components/Header.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,6 @@
-export default function Header(props: {}) {
+type HeaderProps = {
+  // Define expected props here
+};
+export default function Header(props: HeaderProps) {
   return <header>Header</header>;
 }
